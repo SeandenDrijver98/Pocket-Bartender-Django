061948aed859ececc66901fe7ffc24175407ce24
Sean den Drijver <sddrijver@gmail.com>
2021-11-28 21:38:27 +0200

    Updated requirements.txt

--- bartender_backend/settings.py
+++ bartender_backend/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Django Rest Framework
 REST_FRAMEWORK = {
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    # "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
