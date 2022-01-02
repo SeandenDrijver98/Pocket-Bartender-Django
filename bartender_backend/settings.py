73075742475e1919fa42d446b6aad5c3b955aa54
Sean den Drijver <sddrijver@gmail.com>
2022-01-02 10:47:42 +0200

    Added media settings

--- bartender_backend/settings.py
+++ bartender_backend/settings.py
@@ -145,6 +145,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # https: // warehouse.python.org / project / whitenoise /
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
